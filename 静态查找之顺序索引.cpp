d3d1564417573c3f1d22b6143cba1fc9fe316998
toolate041 <1102130058@qq.com>
2025-12-03 11:32:15 +0800

    静态查找之顺序索引

--- 静态查找之顺序索引.cpp
+++ 静态查找之顺序索引.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, k, t;
     cin >> n;
     int arr[1000];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
     cin >> k;
     int indexMax[100]; // 每个块的最大值
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++)
+    {
         cin >> indexMax[i];
     }
 
     int blockSize = n / k; // 每块大小
     int indexStart[100];   // 每块起始位置
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++)
+    {
         indexStart[i] = i * blockSize;
     }
 
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int target;
         cin >> target;
 
@@ -30,15 +35,18 @@ int main() {
         int cnt = 0;
 
         // 索引表顺序查找
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++)
+        {
             cnt++;
-            if (target <= indexMax[i]) {
+            if (target <= indexMax[i])
+            {
                 blockIdx = i;
                 break;
             }
         }
 
-        if (blockIdx == -1) {
+        if (blockIdx == -1)
+        {
             cout << "error" << endl;
             continue;
         }
@@ -47,16 +55,19 @@ int main() {
         int found = 0;
         int start = indexStart[blockIdx];
         int end = start + blockSize;
-        for (int i = start; i < end; i++) {
+        for (int i = start; i < end; i++)
+        {
             cnt++;
-            if (arr[i] == target) {
+            if (arr[i] == target)
+            {
                 cout << i + 1 << "-" << cnt << endl;
                 found = 1;
                 break;
             }
         }
 
-        if (!found) {
+        if (!found)
+        {
             cout << "error" << endl;
         }
     }
